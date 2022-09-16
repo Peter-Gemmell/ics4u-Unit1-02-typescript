db1fb838bf40eeeb66578c75a7fc7ce2147dd495
Peter Gemmell <peter.gemmell@mths.ca~>
2022-09-16 01:06:21 +0000

    willy

---
+++ src/Energy.ts
@@ -0,0 +1,30 @@
+/**
+ * The program calculates the
+ *
+ *
+ * By: Peter Gemmell
+ * Version: 1.0
+ * Since:   2022-09-15
+ */
+
+import promptSync from 'prompt-sync'
+try {
+  const prompt = promptSync()
+  // let logSize: number
+  const speed = (2.998 * 10 ** 8) ** 2
+  const massString = prompt('What is the mass : ')
+  const mass = parseFloat(massString)
+  if (isNaN(mass)) {
+    throw new Error('not a number')
+  }
+  if (mass <= 0) {
+    throw new Error('negative number')
+  }
+
+  const energy = mass * speed
+  console.log(`${energy} Joules would be produced`)
+} catch (e) {
+  console.log('Not an acceptable input')
+}
+
+console.log('\nDone.')
