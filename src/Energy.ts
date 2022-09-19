5f779f7ed6e37503eebd083ece63aed519a3b3c6
Peter Gemmell <peter.gemmell@mths.ca~>
2022-09-19 13:29:09 +0000

    Final

--- src/Energy.ts
+++ src/Energy.ts
@@ -8,11 +8,11 @@
  */
 
 import promptSync from "prompt-sync";
-try {
   const prompt = promptSync();
-  // let logSize: number
   const speed = (2.998 * 10 ** 8) ** 2;
   const massString = prompt("What is the mass : ");
+try {
+  // let logSize: number
   const mass = parseFloat(massString);
   if (isNaN(mass)) {
     throw new Error("not a number");
